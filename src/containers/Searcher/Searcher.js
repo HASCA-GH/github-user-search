271e421a734473306f5faa4ef1af148456ca7a17
Humberto Asca <hasca@ciplima.org.pe>
2023-01-17 10:53:45 -0500

    My first commit

---
+++ src/containers/Searcher/Searcher.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Stack, TextField, IconButton} from '@mui/material'
+
+const Searcher = () => {
+  return (
+    <Stack>Searcher
+      <TextField>
+      </TextField>
+      <IconButton></IconButton>
+    </Stack>
+  )
+}
+
+export default Searcher
