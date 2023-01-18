caca54e88585f357c3d34a8b0aea414c33f58949
Humberto Asca <hasca@ciplima.org.pe>
2023-01-18 14:21:38 -0500

    Connecting the API https://api.github.com/users/octocat and getting the data response

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
+import Searcher from './containers/Searcher/Searcher';
+// import Box from '@mui/material/Box'
 
+import getGitHubUser from './services/users';
 
 function App() {
+
+  const [inputUser, setInputUser] = useState('octocat');
+  const [userState, setUserState] = useState(inputUser);
+  const [notFound, setNotFound] = useState(false);
+
+  const gettingUser = async (user) => {
+    const userResponse = await getGitHubUser(user)
+    // Saving to our local storage
+    if (userState === 'octocat') {
+      localStorage.setItem('octocat', userResponse)
+    }
+
+    if (userResponse.message ==='Not Found') {
+      const {octocat} = localStorage;
+      setInputUser(octocat)
+      setNotFound(true)
+    } else {
+      setUserState(userResponse)
+    }
+    console.log('My userXXXX', userResponse)
+
+    console.log('userState interno >>>> ', userState)
+  }
+  console.log('userState >>>> ', userState)
+  useEffect(() => {
+    gettingUser(inputUser)
+  }, [inputUser])
+  
   return (
 
     <div className="App">
       Inicia aquí
-      <Container maxWidth="sm">
-        Container
-        Hellllllll 
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container 
+          maxWidth="sm"
+          sx={{ bgcolor: 'whitesmoke', 
+          width: '80vh',
+          height: '100vh',
+          borderRadius: '16px', 
+          marginTop: '40px',
+          display: 'flex',
+          flexDirection:'column',
+          alignItems: 'center'
+           }}>
+        <Searcher inputUser={inputUser} setInputUser={setInputUser} />
       </Container>
      
     </div>
--- src/containers/Searcher/Searcher.js
+++ src/containers/Searcher/Searcher.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import {Stack, TextField, IconButton} from '@mui/material'
+import React, { useState } from 'react'
+import {Stack, TextField, IconButton, colors} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+// import { lightGreen } from '@mui/material/colors';
 
-const Searcher = () => {
+
+const Searcher = ({inputUser, setInputUser}) => {
+  const [valueInput, setValueInput] = useState(inputUser)
+
+  const onSearchValueChange = (e) => {
+    const inputValue = e.target.value
+    setValueInput(inputValue)
+    // console.log("inputValue", inputValue);
+  }
+  const handleSubmit = () => {
+    setInputUser(valueInput)
+    // console.log('valueInput', valueInput);
+  }
   return (
-    <Stack>Searcher
-      <TextField>
-      </TextField>
-      <IconButton></IconButton>
+    <Stack 
+      direction= 'row'
+      sx={{
+        marginTop: '30px',
+        width:'80%'
+    }}> 
+      <TextField
+        id="outlined-basic"
+        label="GutHub User"
+        value= {valueInput}
+        variant="outlined"
+        placeholder='Search a GitHub user' 
+        size="small" 
+        onChange={onSearchValueChange}
+        sx={{
+          width: '90%'
+        }}
+      />
+      <IconButton 
+          onClick={handleSubmit}
+          sx={{
+            backgroundColor:'lightGreen',
+            left: '-41px'}}
+      >
+        <SearchIcon/>
+      </IconButton>
+
     </Stack>
   )
 }
